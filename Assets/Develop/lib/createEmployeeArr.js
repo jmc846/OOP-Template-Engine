aadbaa90694c7cff409a706f260048941ba67e24
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-08-06 14:41:05 -0400

    upates image in README

--- Assets/Develop/lib/createEmployeeArr.js
+++ Assets/Develop/lib/createEmployeeArr.js
@@ -1,29 +1,35 @@
 const createEmployeeArr = [
-    {   type: 'checkbox',
+    {
+        type: 'checkbox',
         message: "Want to create an employee?",
-        choices: ['Yes','No']
+        name: "new employee",
+        choices: ['Yes', 'No']
     },
-    { type: 'checkbox',  
-    message: "What is the employee's role",
-    choices: ["Manager","Engineer","Intern"],
-    name: 'employeeRole'
-      
-  },
-     {   type: 'input',
+    {
+        type: 'checkbox',
+        message: "What is the employee's role",
+        name: 'employeeRole',
+        choices: ["Manager", "Engineer", "Intern"],
+        
+
+    },
+    {
+        type: 'input',
         message: "Please provide an Employee ID",
         name: "id"
     },
-    {  
-      type: 'input',  
-      message: "Please provide an Employee email",
+    {
+        type: 'input',
+        message: "Please provide an Employee email",
         name: "email"
     },
-    {    type: 'input',
-    message: "Please provide an Employee name",
-    name: "employeeName",
-      
-    },
-   
-  ]
+    {
+        type: 'input',
+        message: "Please provide an Employee name",
+        name: "employeeName",
 
-  module.exports = createEmployeeArr;
+    },
+
+]
+
+module.exports = createEmployeeArr;
