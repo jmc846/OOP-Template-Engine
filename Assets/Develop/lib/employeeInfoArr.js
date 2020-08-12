21a4803de0a623a7f698578155b0c1358a7f4573
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-08-12 12:11:34 -0400

    infoArr adds

--- Assets/Develop/lib/employeeInfoArr.js
+++ Assets/Develop/lib/employeeInfoArr.js
@@ -1,42 +1,48 @@
 const employeeInfoArr = [
-     {
-         type: 'list',
-         name: "new",
-         message:"Want to create an employee?",      
-         choices: ['Yes', 'No'],
-     },
-
-     {
-        type: 'checkbox',
-        name: 'role',
-        message: "What is the employee's role",        
-        choices: [                  
-                'Manager',          
-                'Engineer',
-                'Intern'             
-        ],      
+    {
+        type: 'list',
+        name: "new team",
+        message: "Please Create an Engineering Team",
+        choices: ['Yes', 'Maybe later'],
     },
-    
+    {
+        type: 'list',
+        name: "new",
+        message: "Want to create an employee?",
+        choices: ['Yes', 'No'],
+    },
+
     {
         type: 'input',
-        name: "id",
-        message: "Please provide an Employee ID",    
-        default: '00000'
+        name: "name",
+        message: "Please provide an Employee name",
+
     },
     {
         type: 'input',
         name: "email",
         message: "Please provide an Employee email",
-        
-    },
-    {
-        type: 'input',
-        name: "name",
-        message: "Please provide an Employee name",
-  
 
     },
-    
+    {
+
+
+        type: 'input',
+        name: "id",
+        message: "Please provide an Employee ID",
+        default: '00000'
+    },
+    {
+        type: 'checkbox',
+        name: 'role',
+        message: "What is the employee's role",
+        choices: [
+            'Manager',
+            'Engineer',
+            'Intern'
+        ],
+    },
+
 
 ]
 
