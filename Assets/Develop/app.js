adcad40d8bd0e2ac73bfcda56eba948e45e77abf
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-08-12 15:24:40 -0400

    changes to render #2

--- Assets/Develop/app.js
+++ Assets/Develop/app.js
@@ -21,7 +21,7 @@ const Employee = require("./lib/Employee");
  const kim = new Intern('kim','23','kim@gmail.com','Rutgers');
  const randy = new Engineer('randy', '30', 'randy@gmail','randy2randy@github.com');
  const employeeArr = [jacob, kim, randy];
-  htmlRender();
+  //htmlRender();
 // console.log(htmlRender(employeeArr))
 //  console.log(jacob);
 //  console.log(kim);
@@ -33,6 +33,20 @@ inquirer.prompt(managerInfoArr).then(function(managerDataResponse){
   console.log(managerDataResponse, managers ++);
   let newManager= new Manager(managerInfoArr)
   let officeNumber = managerDataResponse.officeNumber
+  let addTeamMember = managerDataResponse.addTeamMember
+      switch (addTeamMember){
+        case 'Manager':    
+        createManager();   
+      break;
+        
+        case 'Engineer':
+       createEngineer();   
+      break;
+    
+        case 'Intern':     
+        createIntern();
+      break;
+      }
 });
 
 }
@@ -41,15 +55,43 @@ function createEngineer (){
     console.log(engineerDataResponse.github, engineers ++);
     let newEngineer= new Engineer(engineerInfoArr)
     let github = engineerDataResponse.github
+    let addTeamMember = engineerDataResponse.addTeamMember
+      switch (addTeamMember){
+        case 'Manager':    
+        createManager();   
+      break;
+        
+        case 'Engineer':
+       createEngineer();   
+      break;
+    
+        case 'Intern':     
+        createIntern();
+      break;
+      }
 
   });
-
   }
+
   function createIntern (){
     inquirer.prompt(internInfoArr).then(function(internDataResponse){
       console.log(internDataResponse.university, interns ++);
       let newIntern= new Intern(internInfoArr)
       let university = internDataResponse.university
+      let addTeamMember = internDataResponse.addTeamMember
+      switch (addTeamMember){
+        case 'Manager':    
+        createManager();   
+      break;
+        
+        case 'Engineer':
+       createEngineer();   
+      break;
+    
+        case 'Intern':     
+        createIntern();
+      break;
+      }
 
     });
 
@@ -82,56 +124,56 @@ function onboard() {
    ;     
 
           console.log("html generation has begun, wait one moment");
-          employees.push(employeeD);
-          console.log(employees)
-          if(response.nextE){
-           inputEmployeeInfo();
-          }else{
-           employees.forEach(employee =>{
-              const role = employee.getRole();
-            })
-         }
+        //   employees.push(employeeD);
+        //   console.log(employees)
+        //   if(response.nextE){
+        //    inputEmployeeInfo();
+        //   }else{
+        //    employees.forEach(employee =>{
+        //       const role = employees.getRole();
+        //     })
+        //  }
           };
-          const employeeCreated = employeeInfoArr.createEmployee(role);
+//           const employeeCreated = employeeInfoArr.Render()
  
-         fs.appendFile("team.html",employeeCreated, err =>{
-            if(err){
-              console.log(err);
-            } else{
-              console.log("YAY!!!");
-            }
+//          fs.appendFile("team.html",employeeCreated, err =>{
+//             if(err){
+//               console.log(err);
+//             } else{
+//               console.log("YAY!!!");
+//             }
 
-           }
-         )
+//            }
+//          )
          
-     .catch((err) => {
-      console.log(err);
-     })
-  ;
-     ;
-// // and to create objects for each team member (using the correct classes as blueprints!)
+//      .catch((err) => {
+//       console.log(err);
+//      })
+//   ;
+//      ;
+// // // and to create objects for each team member (using the correct classes as blueprints!)
 
-// // After the user has input all employees desired, call the `render` function (required
-// // above) and pass in an array containing all employee objects; the `render` function will
-// // generate and return a block of HTML including templated divs for each employee!
+// // // After the user has input all employees desired, call the `render` function (required
+// // // above) and pass in an array containing all employee objects; the `render` function will
+// // // generate and return a block of HTML including templated divs for each employee!
 
-// // After you have your html, you're now ready to create an HTML file using the HTML
-// // returned from the `render` function. Now write it to a file named `team.html` in the
-// // `output` folder. You can use the variable `outputPath` above target this location.
-// // Hint: you may need to check if the `output` folder exists and create it if it
-// // does not.
+// // // After you have your html, you're now ready to create an HTML file using the HTML
+// // // returned from the `render` function. Now write it to a file named `team.html` in the
+// // // `output` folder. You can use the variable `outputPath` above target this location.
+// // // Hint: you may need to check if the `output` folder exists and create it if it
+// // // does not.
 
-// // HINT: each employee type (manager, engineer, or intern) has slightly different
-// // information; write your code to ask different questions via inquirer depending on
-// // employee type.
+// // // HINT: each employee type (manager, engineer, or intern) has slightly different
+// // // information; write your code to ask different questions via inquirer depending on
+// // // employee type.
 
-// // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// // and Intern classes should all extend from a class named Employee; see the directions
-// // for further information. Be sure to test out each class and verify it generates an
-// // object with the correct structure and methods. This structure will be crucial in order
-// // for the provided `render` function to work! ```
-// // render();
-   ;  
+// // // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// // // and Intern classes should all extend from a class named Employee; see the directions
+// // // for further information. Be sure to test out each class and verify it generates an
+// // // object with the correct structure and methods. This structure will be crucial in order
+// // // for the provided `render` function to work! ```
+// // // render();
+//    ;  
   
 ;
             ;
